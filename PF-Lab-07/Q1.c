bce6afc806f03252c6fb81b3c88c7c0f99c3de48
Muhammad Owais Raza <mowaisraza430@gmail.com>
2026-03-09 20:35:40 +0500

    Lab Tasks

---
+++ PF-Lab-07/Q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[10];
+    int i, search, count = 0;
+
+   
+    printf("Enter 10 numbers: ");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    
+    printf("Enter the number to search: ");
+    scanf("%d", &search);
+
+    
+    for (i = 0; i < 10; i++) {
+        if (numbers[i] == search) {
+            count++;
+        }
+    }
+
+    
+    if (count > 0) {
+        printf("The number %d occurred %d time(s) in the array.\n", search, count);
+    } else {
+        printf("Number not found.\n");
+    }
+
+    return 0;
+}
